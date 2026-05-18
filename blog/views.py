190b2aea140c668f90bcc9914b0ff5f7b8656b95
seungjin <xeungjin@nate.com>
2026-05-18 15:59:48 +0900

    368page

--- blog/views.py
+++ blog/views.py
@@ -38,8 +38,7 @@ def tag_page(request, slug):
             'post_list': post_list,
             'tag': tag,
             'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects
-            .filter(category=None).count(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
 
